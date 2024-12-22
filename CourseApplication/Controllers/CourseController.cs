1627e9ab3a3f27012fb4a74c513f68075e0be6fb
emrecan <turgul2606@gmail.com>
2024-12-22 18:55:30 +0300

    Add project files.

---
+++ CourseApplication/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApplication.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Apply()
+        {
+            return View();   // burası get için çalışan methoddur
+        }
+        [HttpPost]
+
+        public IActionResult Apply(Candidate model)
+        {
+            return View();   // burası post için çalışan methoddur
+        }
+
+    }
+}
---
+++ CourseApplication/Models/Candidate.cs
@@ -0,0 +1,18 @@
+﻿namespace CourseApplication.Models
+{
+    public class Candidate
+    {
+        public string? Email { get; set; } = string.Empty;
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName?.ToUpper()}"; //sadece get bloğu varmış gibi kullandım 
+        public int? Age { get; set; }
+        public string? SelectedCourse { get; set; } = string.Empty;
+        public DateTime ApplyAt { get; set; }
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
